96bbdf0eeec7d33b093e0abda119ee76bb5c70e6
Mark A Guza <guzamaguza@gmail.com>
2020-05-24 03:36:55 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,68 @@
 class FiguresController < ApplicationController
   # add controller methods
+  get '/figures' do
+    @figures = Figure.all
+    erb :'/figures/index'
+  end
+
+  get '/figures/new' do
+    @landmarks = Landmark.all
+    @titles = Title.all
+    erb :'/figures/new'
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'/figures/show'
+  end
+
+  get '/figures/:id/edit' do
+    #find figure you want to edit
+    @figure = Figure.find(params[:id])
+    #load up all titles
+    @titles = Title.all
+    #load up all landmarks
+    @landmarks = Landmark.all
+
+    erb :'/figures/edit'
+  end
+
+  post '/figures' do
+    binding.pry
+    #create a new figure using params inputs
+    @figure = Figure.create(params[:figure])
+    #if the user put a new title then add it to the figure
+    if !params([:title][:name]).empty?
+      @figure.titles << Title.create(params[:title])
+    end
+    #if the user put a new landmark/year then add it to the figure
+    if !params([:landmark][:name]).empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+    #save the figure to the db
+    @figure.save
+
+    redirect :"/figures/#{@figure.id}"
+  end
+
+  patch '/figures/:id' do
+    #find the figure you will be editing
+    @figure = Figure.find(params[:id])
+    #update the figure
+    @figure.update(params[:figure])
+    #if the title field was filled out then update the title
+    if !params[:title][:name].empty?
+      @figure.titles << Title.create(params[:title])
+    end
+
+    #if the landmark field was filled out then update it
+    if !params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+
+    @figure.save
+
+    redirect :"/figures/#{@figure.id}"
+  end
+
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,38 @@
 class LandmarksController < ApplicationController
-  # add controller methods
+
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :"/landmarks/index"
+  end
+
+  get '/landmarks/new' do
+    erb :"/landmarks/new"
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    erb :"/landmarks/show"
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find(params[:id])
+    erb :"/landmarks/edit"
+  end
+
+  post '/landmarks' do
+    #this comes from the new view so create a new one
+    @landmark = Landmark.create(params[:landmark])
+    @landmark.save
+
+    redirect "/landmarks/#{@landmark.id}"
+  end
+
+  patch 'landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    @landmark.update(params[:landmark])
+    @landmark.save
+
+    redirect "/landmarks/#{@landmark.id}"
+  end
+
 end
