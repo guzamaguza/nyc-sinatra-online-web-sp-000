fcd8eafbafe5949b49b9a7553a4254b120f4adc3
Mark A Guza <guzamaguza@gmail.com>
2020-05-24 03:43:27 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 3) do
+ActiveRecord::Schema.define(version: 2) do
 
   create_table "artists", force: :cascade do |t|
     t.string  "name"
@@ -20,10 +20,6 @@ ActiveRecord::Schema.define(version: 3) do
   end
 
   create_table "figures", force: :cascade do |t|
-    t.string "name"
-  end
-
-  create_table "titles", force: :cascade do |t|
     t.string "name"
   end
 
