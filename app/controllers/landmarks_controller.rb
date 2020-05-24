e71d4ba6d12f7e8f0110a4ffe80c6e5aa54eec70
Mark A Guza <guzamaguza@gmail.com>
2020-05-24 03:52:50 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,38 +1,40 @@
 class LandmarksController < ApplicationController
+  # add controller methods
 
   get '/landmarks' do
     @landmarks = Landmark.all
-    erb :"/landmarks/index"
+    erb :'/landmarks/index'
   end
 
   get '/landmarks/new' do
-    erb :"/landmarks/new"
+    erb :'/landmarks/new'
   end
 
   get '/landmarks/:id' do
     @landmark = Landmark.find(params[:id])
-    erb :"/landmarks/show"
+    erb :'/landmarks/show'
   end
 
   get '/landmarks/:id/edit' do
     @landmark = Landmark.find(params[:id])
-    erb :"/landmarks/edit"
+    erb :'/landmarks/edit'
   end
 
   post '/landmarks' do
-    #this comes from the new view so create a new one
     @landmark = Landmark.create(params[:landmark])
     @landmark.save
-
+  
     redirect "/landmarks/#{@landmark.id}"
   end
 
-  patch 'landmarks/:id' do
+  patch '/landmarks/:id' do
     @landmark = Landmark.find(params[:id])
     @landmark.update(params[:landmark])
-    @landmark.save
+    @landmarks.save
 
     redirect "/landmarks/#{@landmark.id}"
   end
 
+
+  
 end
