54353d00f8f7fefee6f67d6c4ca30c12ad153a6e
Mark A Guza <guzamaguza@gmail.com>
2020-05-24 03:52:07 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,13 +1,17 @@
 class FiguresController < ApplicationController
   # add controller methods
+  class FiguresController < ApplicationController
+  # add controller methods
+
   get '/figures' do
     @figures = Figure.all
-    erb :'/figures/index'
+    erb :'figures/index'
   end
 
   get '/figures/new' do
-    @landmarks = Landmark.all
     @titles = Title.all
+    @landmarks = Landmark.all
+
     erb :'/figures/new'
   end
 
@@ -17,52 +21,54 @@ class FiguresController < ApplicationController
   end
 
   get '/figures/:id/edit' do
-    #find figure you want to edit
     @figure = Figure.find(params[:id])
-    #load up all titles
     @titles = Title.all
-    #load up all landmarks
     @landmarks = Landmark.all
-
     erb :'/figures/edit'
   end
 
   post '/figures' do
-    binding.pry
-    #create a new figure using params inputs
+    # Create new figure from params
     @figure = Figure.create(params[:figure])
-    #if the user put a new title then add it to the figure
-    if !params([:title][:name]).empty?
-      @figure.titles << Title.create(params[:title])
+
+    # if there is a new title, add it to titles & figure
+    if !params[:title][:name].empty?
+      @figure.titles << Title.create(params[:title])     
     end
-    #if the user put a new landmark/year then add it to the figure
-    if !params([:landmark][:name]).empty?
-      @figure.landmarks << Landmark.create(params[:landmark])
+
+    # if there is a new landmark/year, add it to landmarks & figure
+    if !params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])      
     end
-    #save the figure to the db
+
+    # save to database!
     @figure.save
 
     redirect :"/figures/#{@figure.id}"
   end
 
   patch '/figures/:id' do
-    #find the figure you will be editing
+    # Find figure by id
     @figure = Figure.find(params[:id])
-    #update the figure
+   
+    # Update figure
     @figure.update(params[:figure])
-    #if the title field was filled out then update the title
+
+    # check for new title
     if !params[:title][:name].empty?
       @figure.titles << Title.create(params[:title])
     end
 
-    #if the landmark field was filled out then update it
+    # check for new landmark
     if !params[:landmark][:name].empty?
       @figure.landmarks << Landmark.create(params[:landmark])
     end
 
+    # save
     @figure.save
 
-    redirect :"/figures/#{@figure.id}"
+    redirect "/figures/#{@figure.id}"
   end
 
+end
 end
